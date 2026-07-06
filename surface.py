f5ba3cfd1e5a9bae4283878c78d3da24c05fa453
mmdemirtas <muhammedmehtidemirtas@gmail.com>
2026-07-06 10:25:43 +0300

    Update surface.py

--- surface.py
+++ surface.py
@@ -5,18 +5,36 @@ class PlaneSurface:
         self.z = z
 
     def intersect(self, ray):
-        # ray: origin + t * direction
-
         origin_z = ray.position[1]
         direction_z = ray.direction[1]
 
         if direction_z == 0:
-            return None  # paralel
+            return None
 
         t = (self.z - origin_z) / direction_z
 
         if t < 0:
-            return None  # arkada kalıyor
+            return None
 
         hit_point = ray.position + t * ray.direction
         return hit_point
+
+
+class MirrorSurface(PlaneSurface):
+
+    def reflect(self, ray):
+
+        hit = self.intersect(ray)
+
+        if hit is None:
+            return None
+
+        # Aynanın normali (yukarı bakıyor)
+        normal = np.array([0.0, 1.0])
+
+        direction = ray.direction
+        direction = direction / np.linalg.norm(direction)
+
+        reflected = direction - 2 * np.dot(direction, normal) * normal
+
+        return hit, reflected
