efd873ae8bcf70ad67830141269aa7491a4d7666
mmdemirtas <muhammedmehtidemirtas@gmail.com>
2026-07-06 10:24:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 from src.ray import Ray
-from src.surface import PlaneSurface
-import numpy as np
+from src.surface import MirrorSurface
 import matplotlib.pyplot as plt
+import numpy as np
 
-ray = Ray([0, 2], [1, -0.5])
-surface = PlaneSurface(z=0)
+ray = Ray([0,2],[1,-0.5])
 
-hit = surface.intersect(ray)
+mirror = MirrorSurface(z=0)
 
-ray.propagate(5)
+result = mirror.reflect(ray)
 
-h = np.array(ray.history)
+if result is not None:
 
-plt.plot(h[:,0], h[:,1])
+    hit, new_dir = result
 
-if hit is not None:
-    plt.scatter(hit[0], hit[1], color='red', s=80)
+    ray.propagate(np.linalg.norm(hit-ray.position))
+
+    reflected_ray = Ray(hit, new_dir)
+    reflected_ray.propagate(5)
+
+    h1 = np.array(ray.history)
+    h2 = np.array(reflected_ray.history)
+
+    plt.plot(h1[:,0],h1[:,1],label="Incident Ray")
+    plt.plot(h2[:,0],h2[:,1],label="Reflected Ray")
+
+    plt.scatter(hit[0],hit[1],color="red",s=70)
 
 plt.axhline(0)
+
 plt.axis("equal")
 plt.grid()
+plt.legend()
 plt.show()
