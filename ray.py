f71161163ae5f8b85d7895d1c62c34334877f006
mmdemirtas <muhammedmehtidemirtas@gmail.com>
2026-07-07 11:34:30 +0300

    Update ray.py

--- ray.py
+++ ray.py
@@ -1,19 +1,25 @@
 import numpy as np
 
 class Ray:
+    def __init__(self,position,direction):
 
-    def __init__(self, position, direction):
+     self.position = np.array(position, dtype=float)
+     ## konum bilgisi np de float veri dipine çevriliyor. self kullanınca bu artık tüm fonksiyonlarda erişime açık??
 
-        self.position = np.array(position, dtype=float)
+     direction = np.array(direction, dtype=float)
+     ## yönü de float'a çevirdik
 
-        direction = np.array(direction, dtype=float)
+     self.direction = direction / np.linalg.norm(direction) 
+     ## Burada yön vektörünü yön vektörünün uzunluğuna böldük. bu sayede elimizde sadece vektörün yönü bilgisi kaldı.
 
-        self.direction = direction / np.linalg.norm(direction)
+     self.history = [self.position.copy()] 
+     ## copy() yazmasak history ile aynı belleği büyük paylaşırdı. self.history ile ilk pozisyonu kaydetmiş olduk.
 
-        self.history = [self.position.copy()]
+    def propogate(self,distance):
+       self.position = self.direction * distance
+       ## eski konuma yön çarpı aldığımız mesafeyi ekleyip yeni konumumuzu buluyoruz.
 
-    def propagate(self, distance):
+       self.history.append(self.position.copy())
+       ## Geçtiği yeni nokta history'e kaydedildi.
 
-        self.position += self.direction * distance
-
-        self.history.append(self.position.copy())
+## Sonuç olarak: position(şuan bulunduğu nokta) direction(birim yön vektörü) history(geçtiği tüm noktalar)
